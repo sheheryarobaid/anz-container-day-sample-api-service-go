6a16a70d99a83adedc335a28b04aa7c73809b202
sheheryarobaid <sheheryarobaid@gmail.com>
2020-01-31 12:34:07 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Bye Bye WORLD!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
